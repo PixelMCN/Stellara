63e920b8a8fdd59d93061c582413473840f8b930
Pixelis0P <131791502+PixelMCN@users.noreply.github.com>
2025-04-09 17:29:47 +0000

    few changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import nextcord
 from nextcord.ext import commands
-from config import BOT_TOKEN  # Import the bot token from config.py
+from config import BOT_TOKEN  
 
-# Intents are required for the bot to function properly
+
 intents = nextcord.Intents.default()
 intents.message_content = True
 
-# Create a bot instance
+
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 @bot.event
@@ -14,6 +14,7 @@ async def on_ready():
     print(f"Bot is ready! Logged in as {bot.user}")
     print(f"-------------------------------------")
 
+# returns the bot's latency
 @bot.slash_command()
 async def ping(interaction: nextcord.Interaction):
     """Returns the bot's latency."""
@@ -21,11 +22,9 @@ async def ping(interaction: nextcord.Interaction):
     await interaction.response.send_message(f"Pong! Latency: {latency}ms")
 
 
-# Run the bot using the token from config.py
-if BOT_TOKEN:
-    bot.run(BOT_TOKEN)
-else:
-    print("Error: BOT_TOKEN is not set in config.py")
+
+bot.run(BOT_TOKEN)
+
 
 
 
